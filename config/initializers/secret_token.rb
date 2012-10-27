fe02038b2b650243531089b7af35c829306b26ac
Tanner Welsh <tanner@openspectrumstudios.com>
2012-10-27 13:15:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleBlog::Application.config.secret_token = 'e43306c11658b74558f2ad6d8e4866d4a0aed1e7bf1766395b841c25415b56647ee50d9f998be92797c587d88dec614c6cc860f5efd6ec75e663c541bf2bdaca'
