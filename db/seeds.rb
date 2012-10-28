b72de19fe22292084384188c2b23938500c2a94b
Tanner Welsh <tanner@openspectrumstudios.com>
2012-10-28 16:57:25 -0700

    Updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,29 @@
-def Article::fake_title
-  Faker::Lorem.words(rand(5) + 3).tap { |t| t.first.capitalize! }.join(' ')
+class Array 
+  def to_paragraph_tags
+    join("<p></p>").prepend("<p>").concat("</p>")
+  end
 end
 
-def Article::fake_body
-  Faker::Lorem.paragraphs(rand(8) + 3).join("<p></p>").prepend("<p>").concat("</p>")
+module ArticlesComments
+  def self.populate_with_fakes
+    10.times do
+      article = Article.create(title: fake_title, body: fake_article_body)
+
+      rand(5).times { article.comments.create(body: fake_comment) }
+    end
+  end
+
+  def self.fake_title
+    Faker::Lorem.words(rand(5) + 3).tap { |t| t.first.capitalize! }.join(' ')
+  end
+
+  def self.fake_article_body
+    Faker::Lorem.paragraphs(rand(8) + 3).to_paragraph_tags
+  end
+
+  def self.fake_comment
+    Faker::Lorem.paragraphs(rand(2) + 1).to_paragraph_tags
+  end  
 end
 
-10.times do
-  Article.create(title: Article.fake_title, body: Article.fake_body)
-end
+ArticlesComments.populate_with_fakes
